ff8525cdd105230032a44875d6362ceb3961d693
松澤　亜希 <matsuzawaaki@matsuzawaakinoMacBook-Pro.local>
2018-11-09 10:14:29 +0900

    learn props of component

--- src/App.js
+++ src/App.js
@@ -16,19 +16,45 @@ import React from 'react';
 //   }
 // }
 
+// const App = () => {
+//   return (
+//     <div>
+//       <Cat />
+//       <Cat />
+//       <Cat />
+//       <Cat />
+//     </div>
+//   );
+// };
+
+// const Cat = () => {
+//   return <div>Meow!</div>;
+// };
+
 const App = () => {
+  const plofiles = [
+    { name: 'Taro', age: 10 }, 
+    { name: 'Hanako', age: 5 },
+    { name: "NoName"}];
   return (
     <div>
-      <Cat />
-      <Cat />
-      <Cat />
-      <Cat />
+      {plofiles.map((plofiles, index) => {
+        return <User name={plofiles.name} age={plofiles.age} key={index} />;
+      })}
     </div>
   );
 };
 
-const Cat = () => {
-  return <div>Meow!</div>;
+const User = props => {
+  return (
+    <div>
+      Hi! I am {props.name}, and {props.age} years old!
+    </div>
+  );
+};
+
+User.defaultProps = {
+  age: 1
 };
 
 export default App;
